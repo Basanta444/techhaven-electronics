a3d1c76e6568d11a6e2341fa135c5203eac8a342
Basanta Khadka <basantakhadka84@gmail.com>
2026-06-29 17:10:56 +0545

    Updated the tsconfig and firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,21 +1,19 @@
-// src/firebase.ts
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCjDU72qCZuuu5rGQlgKHlxIUvdqWg8CY4",
-  authDomain: "techhaven-electronics-3d0c6.firebaseapp.com",
-  projectId: "techhaven-electronics-3d0c6",
-  storageBucket: "techhaven-electronics-3d0c6.firebasestorage.app",
-  messagingSenderId: "930469654853",
-  appId: "1:930469654853:web:500024bb591ba035088e2c"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export Services for use in other files
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
